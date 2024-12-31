9b070c283e0f645bacd354665d6cb170aef3aa02
HFCS <HFCS@DESKTOP-EMSSPCS>
2024-12-31 02:09:20 +0200

    Add project files.

---
+++ Ass 1 oop/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass_1_oop
+{
+    struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = Y;
+        }
+    
+    //static double CalculateDistance(Point p1, Point p2)
+    //    {
+    //        double deltaX = p2.X - p1.X;
+    //        double deltaY = p2.Y - p1.Y;
+    //        return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+    //    }
+    }
+}
---
+++ Ass 1 oop/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ass_1_oop
+{
+
+
+   
+    internal class Program
+    {
+
+        static double CalculateDistance(Point p1, Point p2)
+        {
+            double deltaX = p2.X - p1.X;
+            double deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+
+        static void Main(string[] args)
+        {
+           
+            
+            
+            Console.Write("Enter the X  of the first point: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Y  of the first point: ");
+            double y1 = double.Parse(Console.ReadLine());
+            // طلب إدخال النقطة الثانية من المستخدم
+            Console.Write("Enter the X  of the second point: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Y  of the second point: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+
+            Point po1 = new Point(x1, y1); 
+            Point po2 = new Point(x2, y2);
+
+            double distance = CalculateDistance(po1, po2);
+            Console.WriteLine($"The distance between the two points is: {distance}");
+
+        }
+    }
+}
